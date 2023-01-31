9c9e080932462a3a2be201564753771e12a2570a
James Thompson <jamesthompson2805@gmail.com>
2023-01-31 07:49:09 +0000

    Edited Ogmo file and playstate to import level Level now mostly rendered, slight hitbox issues Tile 0 does not render currently

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,14 +1,37 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.addons.editors.ogmo.FlxOgmo3Loader;
+import flixel.tile.FlxTilemap;
 
 class PlayState extends FlxState
 {
 	var player:Player;
 
+	var map:FlxOgmo3Loader;
+	var walls:FlxTilemap;
+
 	override public function create()
 	{
-		player = new Player(20, 20);
+		map = new FlxOgmo3Loader(AssetPaths.GameJam1__ogmo, AssetPaths.level1__json);
+
+		//TODO: The first tile (tile 0) does not render
+		walls = map.loadTilemap(AssetPaths.SimpleWalls__png, "Walls");
+		walls.follow();
+		for (i in 0...27)
+		{
+			walls.setTileProperties(i, NONE);
+		}
+		walls.setTileProperties(3, ANY);
+		walls.setTileProperties(4, ANY);
+		walls.setTileProperties(5, ANY);
+		walls.setTileProperties(10, ANY);
+		walls.setTileProperties(19, ANY);
+		add(walls);
+
+		//TODO: Change this to be from the json file
+		player = new Player(96, 96);
 		add(player);
 
 		super.create();
@@ -17,5 +40,6 @@ class PlayState extends FlxState
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		FlxG.collide(player, walls);
 	}
 }
